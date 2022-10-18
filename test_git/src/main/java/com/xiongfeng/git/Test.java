c3b61c83d428dfb3c79bac85219f7fbafed872b2
liuxiongfengfeng <91255100+liuxiongfengfeng@users.noreply.github.com>
2022-10-18 20:04:21 +0800

    Update Test.java

--- test_git/src/main/java/com/xiongfeng/git/Test.java
+++ test_git/src/main/java/com/xiongfeng/git/Test.java
@@ -8,5 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("github test");
+        
     }
 }
