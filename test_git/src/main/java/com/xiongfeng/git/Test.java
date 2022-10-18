9a832d9e02ba07acc70c85108cae1e0e37b3525d
lxf <962537381@qq.com>
2022-10-18 19:20:44 +0800

    hot-fix commit 1

---
+++ test_git/src/main/java/com/xiongfeng/git/Test.java
@@ -0,0 +1,11 @@
+package com.xiongfeng.git;
+
+/**
+ * @author 刘雄锋
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hot-fix test");
+    }
+}
